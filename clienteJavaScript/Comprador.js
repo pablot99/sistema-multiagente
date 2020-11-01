5cd40a62200e98fd57617ac3b3b5c7eccfd8ae7a
NIKOLA SVETLOZAROV DYULGEROV <Nikola.Svetlozarov@alu.uclm.es>
2020-11-01 20:27:34 +0100

    Creada la carpeta y archivos para clientes JavaScript

---
+++ clienteJavaScript/Comprador.js
@@ -0,0 +1,15 @@
+/*  Poned los grupos que modifican este archivo*/
+/*  Este es un constructor que hicimos, añadid los métodos para manejar los datos y atributos que creais convenientes*/
+class Comprador {
+  constructor(ip, puerto, ipMonitor, puertoMonitor) {
+    this.ip = ip;
+    this.puerto = puerto;
+    this.ipMonitor = ipMonitor;
+    this.puertoMonitor = puertoMonitor;
+
+    this.listaCompra = [];
+    this.listaTiendas = [];
+    this.dineroGastado = 0;
+    this.tiempoConsumido = 0;
+  }
+}
