773c3fe57beaee66f6bf1816ce5bb7e3ce63214d
CarlosMorote <carlos.morote1@gmail.com>
2020-11-07 16:00:09 +0100

    Enviar mensajes actualizado

--- clienteJavaScript/EnvioMensajes.js
+++ clienteJavaScript/EnvioMensajes.js
@@ -11,24 +11,39 @@
 function escribirCabecera(infoMensaje){
     
     return  "<head>" + 
-                "<tipo>" +
-                    infoMensaje.tipo +
-                "</tipo>" +
-                "<idcliente>" +
-                    infoMensaje.idCliente + 
-                "</idcliente>" +
-                "<idtienda>" + 
-                    infoMensaje.idTienda +
-                "</idtienda>" +
-                "<time>" +
+                "<tipo_mensaje>" +
+                    infoMensaje.tipo_mensaje +
+                "</tipo_mensaje>" +
+
+                "<tipo_emisor>" +
+                    'comprador' +
+                "</tipo_emisor>" +
+                "<id_emisor>" +
+                    infoMensaje.id_emisor + 
+                "</id_emisor>" +
+                "<ip_emisor>" +
+                    infoMensaje.ip_emisor +
+                "</ip_emisor>" +
+                "<puerto_emisor>" +
+                    '-1' +
+                "</puerto_emisor>" +
+
+                "<tipo_receptor>" +
+                    infoMensaje.tipo_receptor +
+                "</tipo_receptor>" +
+                "<id_receptor>" +
+                    infoMensaje.id_receptor + 
+                "</id_receptor>" +
+                "<ip_receptor>" +
+                    infoMensaje.ip_receptor +
+                "</ip_receptor>" +
+                "<puerto_receptor>" +
+                    infoMensaje.puerto_receptor +
+                "</puerto_receptor>" +
+
+                "<time_sent>" +
                     getTime() +
-                "</time>" +
-                "<IP>" +
-                    infoMensaje.ip + 
-                "</IP>" +
-                "<puerto>" +
-                    infoMensaje.puerto +
-                "</puerto>" +
+                "</time_sent>" +
             "</head>";
 
 }
@@ -36,9 +51,9 @@ function escribirCabecera(infoMensaje){
 // Genera el cuerpo del mensaje XML en funcion del tipo
 function escribirCuerpo(infoMensaje){
 
-    var mensaje = "<body>";
+    var mensaje = "<body xsi:type=\""+infoMensaje.tipo_mensaje+"\">";
 
-    switch (infoMensaje.tipo){
+    switch (infoMensaje.tipo_mensaje){
         case 'entrada_tienda':
             mensaje += entrada_tienda(infoMensaje);
             break;
@@ -60,9 +75,7 @@ function crearMensaje(infoMensaje){
 
     //TODO: Cambiar modelo de mensaje cuando se tenga el completo
     mensaje="<?xml version=\"1.0\" encoding=\"UTF-8\"?>" + 
-            '<?xml-model href="'+infoMensaje.tipo+'.xsd" type="application/xml" schematypens="http://www.w3.org/2001/XMLSchema"?>' +
-            // '<root xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation='+infoMensaje.tipo+'.xsd">' +
-            "<root>" +
+            '<root xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation=\"TodosMensajes.xsd\">' +
                 escribirCabecera(infoMensaje)+
                 escribirCuerpo(infoMensaje)+
             "</root>";
@@ -110,17 +123,31 @@ function entrada_tienda(infoMensaje){
 
 // Genera cuerpo para el mensaje para pedir tiendas TODO
 function pedir_tiendas(infoMensaje){
-
+    var mensajes = "<lista_tiendas>";
+    for(let i=0; i<infoMensaje.tienda.length; i++){
+        mensajes += "<tienda>" +
+                        "<id_tienda>" +
+                            infoMensaje.tienda[i].id +
+                        "</id_tienda>"+
+                        "<ip_tienda>" +
+                            infoMensaje.tienda[i].id +
+                        "</ip_tienda>"+
+                        "<puerto>"+
+                            infoMensaje.tienda[i].puerto +
+                        "</puerto>"+
+                    "</producto>";
+    }
+    return mensajes +="</lista_productos>";
 }
 
 
 //Funcion JQuery ajax para mandar mensajes y recibir respuesta
-function enviarXML(direccion, mensaje, asincrono){
+function enviarXML(direccion, infoMensaje, asincrono){
     var respuesta;
 
     $.ajax({
         url: 'http://' + direccion.replace("http://", "").replace(/\/\//g,"/"),
-        data: mensaje,
+        data: crearMensaje(infoMensaje),
         type: 'POST',
         async: asincrono,
         dataType: 'text',
@@ -148,16 +175,35 @@ function enviarXML(direccion, mensaje, asincrono){
     return respuesta;
 }
 
+// WARNING: IP global, no local
+function get_IP() {
+    var ipCliente;
+	// Funcion jQuery que obtiene la ip de la maquina
+	$.ajax({
+		url: 'https://ipinfo.io/',
+		async: false,
+		dataType: 'json',
+		contentType: 'application/j-son;charset=UTF-8',
+		success: function (data) {
+			ipCliente = data.ip
+		}
+    });
+    return ipCliente;
+}
+
+// https://es.stackoverflow.com/questions/360331/url-en-ajax-jquery-3-5-1
 
 // EJEMPLO DE COMO TIENE QUE RECIBIR LA INFORMACION LOS METODOS
 // DE ESPECIAL INTERES ENVIARXML
 
 // var infoM = {
-//     tipo: 'entrada_tienda',
-//     idCliente: 3,
-//     idTienda: 10,
-//     ip: '198.161.1.1',
-//     puerto: '80',
+//     tipo_mensaje: 'entrada_tienda',
+//     id_emisor: 3,
+//     ip_emisor: '192.168.1.4',
+//     tipo_receptor: 'tienda',
+//     id_receptor: 10,
+//     ip_receptor: '198.161.1.1',
+//     puerto_receptor: '8000',
 //     productos: [
 //         {id: 5, cantidad: 45},
 //         {id: 13, cantidad: 100}
